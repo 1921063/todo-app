aeb5b10f43d75ab2a4d3a7ad384d18dddfb17403
1921063 <osr.mansisharma@gmail.com>
2024-12-13 21:42:26 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export function Header({todos}){
+    const todosLength = todos.length 
+    const isTaskPlural = todos.length !=1
+    const taskOrTasks = isTaskPlural ? 'tasks' : 'task'
+    return(
+        <header>
+            <h1 className="text-gradient">You have {todosLength} open {taskOrTasks}.</h1>
+        </header>
+    )
+}
+
+
+
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+export function TodoInput(props) {
+    const {handleAddtodo} = props
+    const [inputValue, setInputValue] = useState('')
+    
+    return(
+        <div className="input-container">
+            <input value={inputValue} onChange={(e) => {setInputValue(e.target.value)}} placeholder="Add task" />
+            <button onClick={() => {
+                if (!inputValue) {return}
+                handleAddtodo(inputValue)
+                setInputValue('')
+            }}>
+            <i className="fa-solid fa-plus"></i>
+            </button>
+        </div>
+    )
+}
